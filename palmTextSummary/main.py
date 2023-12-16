ef1a40803226d8835fe7cf8cb62a52ee74db10e3
Pranav Bhat <96832303+psbhatbvbcs@users.noreply.github.com>
2023-12-16 08:55:29 +0530

    Scrape news articles

--- palmTextSummary/main.py
+++ palmTextSummary/main.py
@@ -6,7 +6,6 @@ class PalmAI(object):
     def __init__(self, text_content):
         self.text_content = text_content
         palm.configure(api_key=os.environ["GoogleApiKey"])
-
         self.generated_summary = ""
 
     def GenerateSummary(self):
